e29daf2965ad7c7663e82b52ff22ad7a3324dec2
X1kiee <oliverl.lohr@gmail.com>
2024-12-18 18:34:43 +0100

    wheeen

--- Battery_analysis_code/RFB_finding_voltage_vs_time_and_cycles.py
+++ Battery_analysis_code/RFB_finding_voltage_vs_time_and_cycles.py
@@ -33,6 +33,7 @@
 
 
 
+
 from data_extraction_code import * 
 
 time, voltage, energy_charge, energy_discharge, q_discharge, q_charge, cycle_number = result
@@ -48,25 +49,25 @@ ax1.set_ylim(0.75, 3)
 ax1.set_xlim(280, 1215)
 
 # setting axis labels with larger font size
-ax1.set_ylabel("Voltage [V]", weight='bold', fontsize=18)
-ax1.set_xlabel("Time [s]", weight='bold', fontsize=18)
+ax1.set_ylabel("Voltage [V]", weight='bold', fontsize=14)
+ax1.set_xlabel("Time [s]", weight='bold', fontsize=14)
 
 ax2 = ax1.twiny()
-ax2.set_xlabel("Cycle Number", weight='bold', fontsize=18)
+ax2.set_xlabel("Cycle Number", weight='bold', fontsize=14)
 
 # fixing the cycles to times
 ax2.set_xlim(290, 1215)
 ax2.set_xticks([280, 634, 931])
-ax2.set_xticklabels(['1', '2', '3'], fontsize=16)  # larger font size for tick labels
+ax2.set_xticklabels(['1', '2', '3'], fontsize=12)  # adjust tick label font size here
 
 # enabling and customizing minor and major ticks
 ax1.minorticks_on()
-ax1.grid(which='major', linewidth=1.5)
-ax1.grid(which='minor', linewidth=0.5)
+ax1.grid(which='major', linewidth=1)
+ax1.grid(which='minor', linewidth=0.2)
 
-# making tick marks and labels larger
-ax1.tick_params(axis='both', which='major', length=12, width=2, labelsize=16)  # major ticks
-ax1.tick_params(axis='both', which='minor', length=6, width=1)  # minor ticks
-ax2.tick_params(axis='x', which='major', length=12, width=2, labelsize=16)  # twiny ticks
+# make tick marks larger
+ax1.tick_params(axis='both', which='major', length=10, width=1.5, labelsize=12)  # major ticks
+ax1.tick_params(axis='both', which='minor', length=5, width=1)  # minor ticks
+ax2.tick_params(axis='x', which='major', length=10, width=1.5, labelsize=12)  # twiny ticks
 
 plt.show()
